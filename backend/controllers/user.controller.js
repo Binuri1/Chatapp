73ba220a756252d57e91944d5ba3459ab53f029a
Binuri Mindula <binuriwagarachchi@gmail.com>
2024-08-21 00:02:32 +0530

    Update server.js

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,8 +1,14 @@
+import User from "../models/user.model.js";
+
 export const getUsersForSidebar = async(req,res)=> {
 
     try {
 
         const loggedInUserId = req.user._id
+
+        const filteredUsers = await User.find({_id: {$ne: loggedInUserId}}).select("-password");  //not get myself messages and not return passwords
+
+        res.status(200).json(filteredUsers)
         
     } catch (error) {
         console.error("Error in getUsersForSidebar controller", error.message);
